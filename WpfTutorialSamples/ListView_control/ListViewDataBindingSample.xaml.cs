a20e562408689b11e7cf1d4ff53aa9c3ff6922fd
xxx <xxx@163.com>
2017-08-13 02:28:10 +0800

    WpfTutorialSamples-ListView_control提交

---
+++ WpfTutorialSamples/ListView_control/ListViewDataBindingSample.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.ListView_control
+{
+    /// <summary>
+    /// ListViewDataBindingSample.xaml 的交互逻辑
+    /// </summary>
+    public partial class ListViewDataBindingSample : UserControl
+    {
+        public ListViewDataBindingSample()
+        {
+            InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Name = "John Doe", Age = 42 });
+            items.Add(new User() { Name = "Jane Doe", Age = 39 });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13 });
+            lvDataBinding.ItemsSource = items;
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return this.Name + ", " + this.Age + " years old";
+        }
+
+    }
+
+}
---
+++ WpfTutorialSamples/ListView_control/ListViewGridViewSample.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.ListView_control
+{
+    /// <summary>
+    /// ListViewGridViewSample.xaml 的交互逻辑
+    /// </summary>
+    public partial class ListViewGridViewSample : UserControl
+    {
+        public ListViewGridViewSample()
+        {
+            InitializeComponent();
+            List<User1> items = new List<User1>();
+            items.Add(new User1() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+            items.Add(new User1() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+            items.Add(new User1() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });
+            lvUsers.ItemsSource = items;
+        }
+
+    }
+}
