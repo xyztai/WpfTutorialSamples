ee871055c0245b6ef37d0be7d2d4d13fa2feb56d
xxx <xxx@163.com>
2017-08-15 06:20:29 +0800

    TreeView基础使用Demo

---
+++ WpfTutorialSamples/TreeView_control/Common.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTutorialSamples.TreeView_control
+{
+    public class MenuItem
+    {
+        public MenuItem()
+        {
+            this.Items = new ObservableCollection<MenuItem>();
+        }
+
+        public string Title { get; set; }
+
+        public ObservableCollection<MenuItem> Items { get; set; }
+    }
+
+}
---
+++ WpfTutorialSamples/TreeView_control/TreeViewDataBindingSample.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.TreeView_control
+{
+    /// <summary>
+    /// TreeViewDataBindingSample.xaml 的交互逻辑
+    /// </summary>
+    public partial class TreeViewDataBindingSample : UserControl
+    {
+        public TreeViewDataBindingSample()
+        {
+            InitializeComponent();
+            MenuItem root = new MenuItem() { Title = "Menu" };
+            MenuItem childItem1 = new MenuItem() { Title = "Child item #1" };
+            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.1" });
+            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.2" });
+            root.Items.Add(childItem1);
+            root.Items.Add(new MenuItem() { Title = "Child item #2" });
+            trvMenu.Items.Add(root);
+        }
+    }
+}
